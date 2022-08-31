a3ccf6f41d686093ffbdef94b8823ef0ae6d3c64
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2022-08-31 12:51:03 -0700

    Time: 1 ms (89.90%), Space: 43.5 MB (34.33%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -3,12 +3,12 @@ class Solution {
         List<List<Integer>> subset = new ArrayList<>();
         subset.add(new ArrayList<>());
         for ( int currentNum : nums){
-        int n = subset.size();
-        for ( int i = 0; i < n; i++){
-            List<Integer> set = new ArrayList<>(subset.get(i));
-            set.add(currentNum);
-            subset.add(set);
-        }
+            int n = subset.size();
+            for( int i =0; i < n; i++){
+                List<Integer> set = new ArrayList<>(subset.get(i));
+                set.add(currentNum);
+                subset.add(set);
+            }
         }
         return subset;
     }
