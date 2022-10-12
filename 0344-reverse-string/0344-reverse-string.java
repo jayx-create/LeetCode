715ab1cf6ff831177c98a6310866eb3cb0e996a1
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2022-10-12 11:55:05 -0700

    Time: 1 ms (99.89%), Space: 54.6 MB (58.83%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,12 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
-        // Use a two pointer teq and keep iterating until they do not cross
-        // we will use a temp variable to hold the value of left then have right //intake it
-        // we will keep repeating until while loop stops
-        
         int left = 0;
         int right = s.length - 1;
-        
         while( left < right){
             char temp = s[left];
             s[left++]=s[right];
