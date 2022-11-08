2371e48e7776e733bfb5c8c3eae0a37e20ed9acd
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2022-11-07 18:40:54 -0800

    Time: 0 ms (100.00%), Space: 54.7 MB (21.07%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,16 +1,12 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // We will use binary search just to find the target. We will initialize two variables 
-        // left and right then we will initialize another variable for mid to find the target
-        // when it comes to binary search we will use a while loop to keep the 
-        // binary search going
-        int left = 0;
+        int left=0;
         int right = nums.length - 1;
-        while( left <= right){
-            int mid = left  + ( right - left) / 2;
-            if( nums[mid] == target ) return mid;
-            else if( nums[mid] > target ) right = mid - 1;
-            else left = mid + 1;
+        while( left <=right ){
+            int mid = left + (right-left) / 2;
+            if( nums[mid]==target) return mid;
+            else if(nums[mid]<target) left=mid + 1;
+            else right = mid - 1;
         }
         return -1;
     }
